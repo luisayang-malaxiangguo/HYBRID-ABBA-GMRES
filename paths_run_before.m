86e487f85a686c8b64d58cf2cd06482fb7bbf924
luisayang <luisa.yang@univ.ox.ac.uk>
2025-12-09 08:48:20 +0000

    Remove all project files from repo (keep local copies)

--- paths_run_before.m
+++
@@ -1,8 +0,0 @@
-% Assuming your project root is the current folder (change if needed)
-projectRoot = pwd;
-
-addpath(genpath(fullfile(projectRoot, 'regu')));
-addpath(genpath(fullfile(projectRoot, 'IRtools')));
-addpath(genpath(fullfile(projectRoot, 'AIRToolsII')));
-% If you have your own code folder, include it too. e.g.:
-% addpath(genpath(fullfile(projectRoot, 'src')));
